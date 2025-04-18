c943ddb09a9b970ef137623735a3c46cb1311dca
Marco Graziotti <marco.graziotti@codingops.dev>
2025-04-18 18:37:01 +0200

    Added the first API to get from the mock all the products

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../../models/product';
 @Component({
   selector: 'app-product-list',
-  imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
+  standalone: false
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  
+products: Product[] = [];
+
+ constructor(private productService: ProductService){}
+
+ // The list of product is loaded OnInit
+  ngOnInit(): void {
+
+    // Get products async from the service
+    this.productService.getProducts().subscribe(prods => {
+      this.products = prods;
+    });
+  }
 
 }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  // Get from the environment file
+  private apiUrl = environment.apiUrl + "/products";
+
+  constructor(private http: HttpClient) { }
+
+  // Get all products
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl)
+  }
 }
